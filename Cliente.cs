bfc96843bc687e70540cb4c002a2de21eeb238f1
Kamehametroll <34353754+Kamehametroll@users.noreply.github.com>
2019-02-01 19:04:08 -0400

    Primera clase de clientes

---
+++ Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrador //Cambiar el namespace al nombre del proyecto
+{
+    public class Cliente
+    {
+        public string codigo;
+        public string Nombres;
+        public string Apellidos;
+        public decimal MontoR; //Monto Retiro
+        public decimal MontoD; //Monto depósito
+        public decimal Balance;
+    }
+}
